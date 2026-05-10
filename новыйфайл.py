17c51a38d6554b2ede0e35fc3ef1e312a610fbbe
ksemon9 <hotlided@gmail.com>
2026-05-10 23:30:00 +0300

    Update новыйфайл.py

--- новыйфайл.py
+++ новыйфайл.py
@@ -2283,4 +2283,4 @@ def handle_leave(data):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, async_mode='eventlet')
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
